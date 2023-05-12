92e327860405a2e6b3b990bd09ee14d7f0b66d47
ACE-47 <haidermohammed66233@gmail.com>
2023-05-12 08:10:47 +0300

    init2

--- authapp/urls.py
+++ authapp/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home,name='Home')
+    path('',views.home,name='Home'),
+    path('sign-up',views.signup,name='sign-up')
 ]
